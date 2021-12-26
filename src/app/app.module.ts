ebdf707fafcab8b526f6bd2c3197b10a02e267d2
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2021-12-26 15:16:49 +0600

    Стилизация компонентов new-message, messages, message-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { MessagesComponent } from './home/messages/messages.component';
+import { NewMessageComponent } from './home/new-message/new-message.component';
+import { MessageItemComponent } from './home/messages/message-item/message-item.component';
+import { NotFoundComponent } from './not-found.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    MessagesComponent,
+    NewMessageComponent,
+    MessageItemComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
