5ed796d8846c087f6ee88427e6b9d72224c62af8
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-01-04 11:18:03 +0600

    Добаление спиннера при загрузке и отправке сообщений

--- src/app/home/new-message/new-message.component.ts
+++ src/app/home/new-message/new-message.component.ts
@@ -1,24 +1,35 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MessagesService } from '../../shared/messages.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-new-message',
   templateUrl: './new-message.component.html',
   styleUrls: ['./new-message.component.css']
 })
-export class NewMessageComponent implements OnInit {
+export class NewMessageComponent implements OnInit, OnDestroy {
   @ViewChild('messageForm') messageForm!: NgForm;
+  isSending: boolean = false;
+  sendingSubscription!: Subscription;
+
 
   constructor(
     private messagesService: MessagesService,
   ) { }
 
   ngOnInit(): void {
+    this.sendingSubscription = this.messagesService.isSendingMessage.subscribe((isSending: boolean) => {
+      this.isSending = isSending;
+    })
   }
 
   onSubmit() {
     this.messagesService.sendMessage(this.messageForm.value.author, this.messageForm.value.message);
+  }
+
+  ngOnDestroy() {
+    this.sendingSubscription.unsubscribe();
   }
 
 }
--- src/app/shared/messages.service.ts
+++ src/app/shared/messages.service.ts
@@ -1,8 +1,8 @@
 import { Message } from './message.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -10,43 +10,71 @@ import { map } from 'rxjs/operators';
 })
 export class MessagesService {
   messages: Message[] = [];
-  fetchingMessages = new Subject<boolean>();
   messagesChange = new Subject<Message[]>();
+  fetchingMessages = new Subject<boolean>();
+  isSendingMessage = new Subject<boolean>();
 
   constructor(
     private http: HttpClient,
-  ) {}
+  ) {
+  }
 
   fetchMessages() {
+    let lastDatetime: string;
     this.fetchingMessages.next(true);
     this.http.get<Message[]>('http://146.185.154.90:8000/messages').pipe(
       map(messages => {
         this.fetchingMessages.next(false);
+        lastDatetime = messages[messages.length - 1].datetime;
         return messages;
-      }, () => {
       })).subscribe(messages => {
-        this.messages = messages;
-        this.sortMessagesByDate();
-        this.messagesChange.next(this.messages);
+      this.messages = messages;
+      this.sortMessagesByDate();
+      this.messagesChange.next(this.messages);
     }, () => {
       this.fetchingMessages.next(false);
     });
+
+    const interval = setInterval(() => {
+      let currentDatetime: string;
+      this.http.get<Message[]>(`http://146.185.154.90:8000/messages`).pipe(
+        map(messages => {
+          currentDatetime = messages[messages.length - 1].datetime;
+          this.fetchingMessages.next(false);
+          if (lastDatetime !== currentDatetime) {
+            this.fetchingMessages.next(true);
+            lastDatetime = currentDatetime;
+            return messages;
+          }
+          return this.messages;
+        })).subscribe(messages => {
+        this.messages = messages;
+        this.sortMessagesByDate();
+        this.fetchingMessages.next(false);
+        this.messagesChange.next(this.messages);
+      }, () => {
+        this.fetchingMessages.next(false);
+      });
+    }, 2000);
   }
 
   sortMessagesByDate() {
     this.messages.sort((currentMessage: Message, nextMessage: Message) => {
-      return  <any>new Date(nextMessage.datetime) - <any>new Date(currentMessage.datetime);
-    })
+      return <any>new Date(nextMessage.datetime) - <any>new Date(currentMessage.datetime);
+    });
   }
 
   sendMessage(author: string, message: string) {
-    let body = new URLSearchParams();
-    body.set('author', `${author}`);
-    body.set('message', `${message}`);
-    this.http.post('http://146.185.154.90:8000/messages', body).subscribe(result => {
-      console.log(result);
-    })
-    this.fetchMessages();
+    this.isSendingMessage.next(true);
+    const body = new HttpParams()
+      .set('author', author)
+      .set('message', message);
+    this.http.post('http://146.185.154.90:8000/messages', body).pipe(
+      tap(() => {
+        this.isSendingMessage.next(false);
+      }, () => {
+        this.isSendingMessage.next(false);
+      })).subscribe();
   }
 
 }
